147b5cc924c080b75b0a9137c61437768b6008ba
K-Cloud25 <keyuroak@gmail.com>
2024-08-15 18:17:34 +0530

    database_setup

---
+++ deliverables/DB_SETUP.sql
@@ -0,0 +1,39 @@
+-- Database Setup
+CREATE DATABASE IF NOT EXISTS havenhive;
+USE havenhive;
+
+-- Employee Table
+CREATE TABLE IF NOT EXISTS employee (
+    empID INT PRIMARY KEY AUTO_INCREMENT ,	-- DB defined ID's
+    empName VARCHAR(50) NOT NULL, 
+    email VARCHAR(150),
+    phone VARCHAR(10),
+    empType INT NOT NULL DEFAULT 3,	-- By Default Employee is Member 
+    passwd VARCHAR(255) NOT NULL	-- Time Being Not Hashed Plain Txt Passwd
+);
+
+CREATE TABLE IF NOT EXISTS credit (
+	empID INT PRIMARY KEY,			-- Should only map to user with Manager Type 
+    credit INT NOT NULL DEFAULT 2000
+);
+
+CREATE TABLE IF NOT EXISTS room (
+	roomID INT PRIMARY KEY AUTO_INCREMENT,
+    roomType VARCHAR(60) NOT NULL, -- RoomID-RoomTypeName 
+    seatingCapacity INT NOT NULL-- Only Admin Modify
+);
+
+CREATE TABLE IF NOT EXISTS amenity(
+	amenityName varchar(40),
+    costPerHour INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS meeting (
+    roomID INT,
+    managerID INT,
+    startTime DATETIME, 	-- DATE TIME -- YYYY-MM-DD HH:MI:SSproduct
+    endTime DATETIME,	-- DATE TIME -- YYYY-MM-DD HH:MI:SS
+    costOfMeeting INT NOT NULL,
+    descrip VARCHAR(255) NOT NULL -- {pro:0,wifi:0,con:0,white:0,water:0,tv:0,coff:0} 
+);
+
