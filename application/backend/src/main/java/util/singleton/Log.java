7e343e93f2f92f240c0c7604bb6e80343d1da182
K-Cloud25 <keyuroak@gmail.com>
2024-08-16 11:11:23 +0530

    Errors Tracking static write function

--- application/backend/src/main/java/util/singleton/Log.java
+++ application/backend/src/main/java/util/singleton/Log.java
@@ -9,10 +9,24 @@ import java.time.format.DateTimeFormatter;
 public class Log {
 
     public final static String logFilePath = "src/main/resources/logs.txt";
+    public final static String errorFilePath = "src/main/resources/errors.txt";
     public final static String dateTimeFormat = "dd-MM-yyyy a k:mm:ss:n";
 
     public static void writeToLog(String logStmt){
         try(BufferedWriter bf = new BufferedWriter(new FileWriter(logFilePath, true))){
+
+            String logStatement = LocalDateTime.now().format(DateTimeFormatter.ofPattern(dateTimeFormat)) + " : "+  logStmt;
+            bf.append(logStatement);
+            bf.newLine();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static void writeToError(String logStmt){
+        try(BufferedWriter bf = new BufferedWriter(new FileWriter(errorFilePath, true))){
 
             String logStatement = LocalDateTime.now().format(DateTimeFormatter.ofPattern(dateTimeFormat)) + " : "+  logStmt;
             bf.append(logStatement);
