38b712b773d350c14736dc6a86014c28c2247451
K-Cloud25 <keyuroak@gmail.com>
2024-08-16 09:17:13 +0530

    Log SingleTon and Logs.txt

---
+++ application/backend/src/main/java/Main.java
@@ -0,0 +1,11 @@
+import util.singleton.Log;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Log.writeToLog("HELLO WORLD2");
+
+    }
+
+}
---
+++ application/backend/src/main/java/util/singleton/Log.java
@@ -0,0 +1,27 @@
+package util.singleton;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Log {
+
+    public final static String logFilePath = "src/main/resources/logs.txt";
+    public final static String dateTimeFormat = "dd-MM-yyyy a k:mm:ss:n";
+
+    public static void writeToLog(String logStmt){
+        try(BufferedWriter bf = new BufferedWriter(new FileWriter(logFilePath, true))){
+
+            String logStatement = LocalDateTime.now().format(DateTimeFormatter.ofPattern(dateTimeFormat)) + " : "+  logStmt;
+            bf.append(logStatement);
+            bf.newLine();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+}
