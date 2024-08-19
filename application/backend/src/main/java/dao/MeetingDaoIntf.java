a59dbbe396fd4175227444b8be4a8ec3438209c5
Vaidehibelsare <Vaidehi.b23902@gmail.com>
2024-08-19 22:40:35 +0530

    Model and dao first commit

---
+++ application/backend/src/main/java/dao/MeetingDaoIntf.java
@@ -0,0 +1,13 @@
+package dao;
+
+import models.entities.Amenity;
+import models.entities.Meeting;
+
+import java.util.List;
+
+public interface MeetingDaoIntf {
+    public void bookMeeting(int meetingId, int employeeId, List<Amenity>amenities);
+    public double calculateRatePerHour(Meeting meeting);
+    Meeting getMeetingById(int meetingId);
+    List<Meeting> getAllMeetings();
+}
